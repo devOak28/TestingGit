50a7bb1fd1009bdaaba081b372df338ef367120f
devOak28 <97941583+devOak28@users.noreply.github.com>
2023-11-15 15:46:09 +0530

    added by devesh

--- AML/src/com/app/Test.java
+++ AML/src/com/app/Test.java
@@ -3,7 +3,7 @@ package com.app;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Main");
+		System.out.println("Hello Github world this is devesh...");
 	}
 
 }
